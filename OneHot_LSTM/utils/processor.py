2b7ede3e58d2e30af3b1b4599675538246cd9b04
mojgd <vazifedoost.m12@gmail.com>
2022-08-16 16:29:34 +0200

    requirements added

--- OneHot_LSTM/utils/processor.py
+++ OneHot_LSTM/utils/processor.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
-import datetime
-from multiprocessing import Pool
+# import datetime
+# from multiprocessing import Pool
 from sklearn.model_selection import train_test_split
 import random
 
